663dbedaa10d3c45e2a4b420293eb781667920f3
Matt Wright <matthewbwright@gmail.com>
2019-10-15 16:36:38 -0700

    adds string interpolation to fill in the blanks, and prints the story

--- MadLibs.cs
+++ MadLibs.cs
@@ -54,7 +54,7 @@ namespace MadLibs
 
             // The template for the story:
 
-            string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
+            string story = $"This morning {name} woke up feeling {adj1}. 'It is going to be a {adj2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adj3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.";
 
 
       // Print the story:
